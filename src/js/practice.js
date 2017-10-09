f57b73cc1ab5234e632450faf1f9143a9bfd1607
Yu Tsuchiya <yutsuchiya@Yu-no-MacBook-Pro.local>
2017-10-09 22:26:32 +0900

    レスポンシブ対応

--- src/js/practice.js
+++ src/js/practice.js
@@ -3,11 +3,20 @@ $(document).ready(function(){
     var slider_nav = $('.practice_client_slide_nav_container');
 
     slider_nav.slick({
+        arrows: false,
         autoplay: true,
         slidesToShow: 3,
         focusOnSelect: true,
         centerMode: true,
-        centerPadding: '5px',
-        swipeToSlide: true
+        swipeToSlide: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '40px'
+                }
+            }
+        ],
     });
 });
